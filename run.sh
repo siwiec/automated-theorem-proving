55879779d000bf3380e836580578fba6105c9567
Mateusz Siwiec <mat.siwiec@gmail.com>
2019-06-23 18:15:21 -0400

    Equivalence check for queries 1 & 2

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-./vampire4.2.2 --mode casc -t 300 ./tptp/equiv12.tptp
+./vampire4.2.2 --mode casc -t 1000 ./tptp/equiv12.tptp
+#read -n1 -r -p "Press any key to continue..." key
+#./vampire4.2.2 --mode casc_sat -t 1000 ./tptp/equiv34.tptp
