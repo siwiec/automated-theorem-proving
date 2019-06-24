6059b16afac664f1ff981ed9b3809e5eb2a8f76f
Mateusz Siwiec <mat.siwiec@gmail.com>
2019-06-24 08:14:39 -0400

    Output added in README.md

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -x
 ./vampire4.2.2 --mode casc -t 1000 ./tptp/equiv12.tptp
 #read -n1 -r -p "Press any key to continue..." key
 #./vampire4.2.2 --mode casc_sat -t 1000 ./tptp/equiv34.tptp
