c428070e7305e02470442db0dac6c79ecde08160
Mateusz Siwiec <mat.siwiec@gmail.com>
2019-07-11 13:53:29 -0400

    Equivalence check added

--- src/Main.hs
+++ src/Main.hs
@@ -20,32 +20,28 @@ main :: IO ()
 main = do
     args <- getArgs
     case args of
-        [] -> do
-              -- read from stdin
-              c <- getContents
-              doIt c
-        ["-s", sql] -> do
-              -- parse arg given
-              doIt sql
-        [f] ->
-              -- read file
-              withFile f ReadMode (\h -> do
-                  x <- hGetContents h
-                  doIt x)
+        ("-t":xs) -> do
+            mapM_ (\x -> do
+                putStrLn $ "Running test for file " ++ x ++ "...\n"
+                withFile x ReadMode (\k -> do
+                    sql <- hGetContents k
+                    runTest sql)
+                runTest x) xs
+        [databaseSchemeFile, query1File, query2File] ->
+            withFile databaseSchemeFile ReadMode (\k -> do
+                databaseScheme <- hGetContents k
+                withFile query1File ReadMode (\l -> do
+                    query1 <- hGetContents l
+                    withFile query2File ReadMode (\m -> do
+                        query2 <- hGetContents m
+                        runEquivalenceCheck databaseScheme query1 query2)))
         _ -> do
-            putStrLn "use no arguments to stream sql from stdin, e.g.:\n\
-                     \  cat some.sql | SimpleSQLParserExample\n\
-                     \n\
-                     \use -s to parse sql on command line, e.g.:\n\
-                     \  SimpleSQLParserExample -s \"select * from t\"\n\
-                     \use a single arg to parse a file, e.g.\n\
-                     \  SimpleSQLParserExample some.sql"
+            putStrLn "Usage: automated-theorem-proving databaseScheme query1 query2"
 
-
-doIt :: String -> IO ()
-doIt src = do
+runTest :: String -> IO ()
+runTest sql = do
     let parsed :: Either ParseError [Statement]
-        parsed = parseStatements ansi2011 "" Nothing src
+        parsed = parseStatements ansi2011 "" Nothing sql
     case parsed of
         (Left err) -> (error . peFormattedError) err
         (Right ast) -> do
@@ -55,3 +51,8 @@ doIt src = do
                     putStrLn err
                     exitWith $ ExitFailure 1
                 (Right tptp) -> putStrLn tptp >> exitSuccess
+
+runEquivalenceCheck :: String -> String -> String -> IO ()
+runEquivalenceCheck databaseScheme query1 query2 = do
+    putStrLn "ERROR"
+    exitWith $ ExitFailure 1
